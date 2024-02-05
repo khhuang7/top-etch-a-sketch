d6c9c03eed3fbdab569674129fdbed6656b1f5d6
Kat <63946129+khhuang7@users.noreply.github.com>
2024-02-05 14:51:32 -0500

    Add button functionality to set number of squares in grid

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const container = document.querySelector(".container");
-
-// Create CSS class for each square div
-var style = document.createElement('style');
-style.innerHTML = `.pixel {\n
-  width: ${90 / 16}vw; \n
-  }`;
-document.head.appendChild(style);
+const style = document.createElement('style');
 
 // Create a webpage with a 16x16 grid of square divs
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-
-  for (let j = 0; j < 16; j++) {
-    const div = document.createElement('div');
-    div.className = 'pixel';
-    div.addEventListener('mouseenter', highlight);
-    row.appendChild(div);
-  }
-
-  container.appendChild(row);  
+function generateGrid(squares) {
+  container.replaceChildren([]);
+  style.innerHTML = `.pixel {\n
+    width: ${90 / squares}vmin; \n
+    }`;
+  document.head.appendChild(style);
+  
+  for (let i = 0; i < squares; i++) {
+    const row = document.createElement("div");
+  
+    for (let j = 0; j < squares; j++) {
+      const div = document.createElement('div');
+      div.className = 'pixel';
+      div.addEventListener('mouseenter', highlight);
+      row.appendChild(div);
+    }
+  
+    container.appendChild(row);  
+  }  
 }
 
 function highlight() {
@@ -27,3 +29,23 @@ function highlight() {
   }
 }
 
+// Use button to ask user to set the grid size
+const setBtn = document.querySelector("button");
+setBtn.addEventListener('click', () => {
+  let message = "How many squares would you like to generate the grid with? (Please enter an integer between 1-100.)"
+  let valid = false;
+  let squares = 0;
+  while (!valid) {
+    let input = prompt(message, 30);
+    if (input === null) {
+      break;
+    } else {
+      squares = parseInt(input);
+      if (squares >= 1 && squares <= 100) {
+        valid = true;
+      } else {
+        message = "Invalid input - please enter an integer between 1-100. How many squares would you like to generate the grid with?"
+      }
+    }
+  if (valid) generateGrid(squares);
+}})
